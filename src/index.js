dd7e207c7168b26bbdcbfd0d8e8585ec7e118f9d
Angel-Edward <141550413+Angel-Edward@users.noreply.github.com>
2023-08-20 23:06:10 +0200

    changed API key

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  let apiKey = "6d1e8e30b5ed19d2791627ab7d7d3405";
+  let apiKey = "eae061c95483dd066657bfc7525418ed";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -97,7 +97,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "6d1e8e30b5ed19d2791627ab7d7d3405";
+  let apiKey = "eae061c95483dd066657bfc7525418ed";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
